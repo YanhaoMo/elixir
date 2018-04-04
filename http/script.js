54fd78684aca9001c9fe2cc721bd4cb684cb2436
Yanhao Mo <yanhaocs@gmail.com>
2018-04-04 15:02:35 +0800

    update gitignore

--- http/script.js
+++ http/script.js
@@ -144,7 +144,7 @@ function offsetAnchor(e) {
     var el = document.querySelector(location.hash)
     if (el) {
       var offsetTop = el.offsetTop
-      wrapper.scrollTop = offsetTop < 100 ? 200 : offsetTop + 100
+      wrapper.scrollTop = offsetTop - 100;
     }
   }
 }
